91d7f558fd20c7d3f78f96ee46fa01fbc111a164
rishij-at-803992426592 <rishi18304@iiitd.ac.in>
2020-07-20 03:50:34 +0530

    Added samplegraphql API

--- App.tsx
+++ App.tsx
@@ -1,23 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Text, View } from 'react-native';
+// import useCachedResources from './hooks/useCachedResources';
+// import useColorScheme from './hooks/useColorScheme';
+// import Navigation from './navigation';
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
 
-export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache()
+});
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
+const EXCHANGE_RATES= gql`
+  query GetRates {
+    rates (currency: "USD") {
+      currency
+    }
+  }
+`;
+
+function ExchangeRates() {
+  const { loading, error, data } = useQuery(gql`
+    {
+      rates(currency: "USD") {
+        currency
+        rate
+      }
+    }
+  `);
+
+  if (loading) return <Text>Loading...</Text>;
+  if (error) return <Text>Error :(</Text>;
+
+  return data.rates.map(({ currency, rate }) => (
+    <View key={currency}>
+      <Text>
+        {currency}: {rate}
+      </Text>
+    </View>
+  ));
+}
+
+class App extends React.Component{
+
+  componentDidMount(){}
+  // const isLoadingComplete = useCachedResources();
+  // const colorScheme = useColorScheme();
+  render(){
+    return  <SafeAreaProvider>
+        <ApolloProvider client={client}>
+          <ExchangeRates />
+        {/* <Navigation colorScheme={colorScheme} />
+          <StatusBar /> */}
+        </ApolloProvider>
+    </SafeAreaProvider>
   }
 }
+
+export default App;
